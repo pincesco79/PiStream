7457375b7e97d04404d05d72fc341e62d66b03c1
pincesco79 <35397577+pincesco79@users.noreply.github.com>
2020-03-06 17:07:36 +0100

    Update pistream.sh

--- pistream.sh
+++ pistream.sh
@@ -13,3 +13,5 @@ sudo iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j DNAT --to-desti
 # This one takes the UDP stream from an LKV373 video HDMI extender and put into a UDP rdp host port 5000 
 gst-launch-1.0 udpsrc uri=udp://192.168.0.254:5004 ! udpsink host=192.168.1.241 port=5000 -vv
 
+#omxh264enc ! 'video/x-h264, streamformat=(string)byte-stream' ! h264parse ! flvmux streamable=true name=mux 
+    gst-launch-1.0 videotestsrc is-live=1 ! videoconvert ! 'video/x-raw, format=(string)I420, width=(int)640, height=(int)480, framerate=25/1' ! queue ! omxh264enc ! 'video/x-h264, streamformat=(string)byte-stream' ! h264parse ! flvmux streamable=true name=mux ! rtmpsink location="rtmp://a.rtmp.youtube.com/live2/xxxx-xxxx-xxxx-xxxx" audiotestsrc ! voaacenc bitrate=128000 ! mux.
